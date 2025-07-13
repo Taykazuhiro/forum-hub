afcaa14c46e5057a7536c10880e3a20a8c3b0553
Taykazuhiro <taykazuhiro@gmail.com>
2025-07-13 19:26:27 -0300

    projeto finalizado

--- src/main/java/br/com/alura/forum_hub/infra/exceptions/TratadorDeErros.java
+++ src/main/java/br/com/alura/forum_hub/infra/exceptions/TratadorDeErros.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,10 +28,6 @@ public class TratadorDeErros {
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity tratarErro500(Exception ex){
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " + ex.getLocalizedMessage() + ex.getMessage());
-    }
 
     @ExceptionHandler(ValidacaoException.class)
     public ResponseEntity tratarErroRegraDeNegocio(ValidacaoException ex){
